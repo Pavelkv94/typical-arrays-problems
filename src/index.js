ded67ff932c26c733741bed205c7d157a1bad495
Pavelkv94 <pavel.kv94@yandex.by>
2020-09-29 20:53:14 +0300

    init: start Typical Array Problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  if (!array || array.length === 0) {
+    return 0;
+} else
+{ let min = array[0];
+    for (i = 0; i < array.length; i++) {
+        if (array[i] < min) {
+            min = array[i]
+    }
+} return min;
 }
+};
 
-exports.max = function max (array) {
-  return 0;
-}
 
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (!array || array.length === 0) {
+        return 0;
+    } else {
+        let max = array[0];
+        for (var i = 1; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            }
+        }
+        return max;
+    }
+};
+
+exports.avg = function avg(array) {
+    if (!array || array.length === 0) {
+        return 0;
+    } else {
+        let x = 0;
+        for (i = 0; i < array.length; i++) {
+            x += array[i];
+        }
+        return x / array.length;
+    }
+};
